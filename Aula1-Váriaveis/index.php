9952fc364e294472b30e5cbd603a330d20c66637
Guilherme-Santt <guisant@icloud.com>
2023-12-13 14:38:24 -0300

    Conteúdo & aula finalizado.

---
+++ Aula1-Váriaveis/index.php
@@ -0,0 +1,27 @@
+<?php
+//declarando váriavel
+$nome = "Guilherme";
+$int = 1000;
+$preco = 10.50;
+//array
+$lista = ["a", 1, "c"];
+$listaarray = array(10, 20, 30, "a");
+$verdade = true;
+$false = false; 
+$variavel10 = 10;
+//declarar váriavel
+echo $nome; 
+echo $int;
+echo $preco;
+echo $lista;
+echo $listarray;
+echo $variavel10;
+
+// mostrar tipo de várivel e conteudo
+var_dump($inteiro);
+
+//constantes
+define('pi',3.14);
+echo pi;
+
+?>
