d753fc55fb8f8224a4c84314c99eafd3b273114e
Guilherme-Santt <guisant@icloud.com>
2023-12-13 16:50:23 -0300

    Conteudo aula Operadores

---
+++ Aula12 Seção 2-Operadores/index.php
@@ -0,0 +1,30 @@
+<?php
+
+// operadores aritméticos
+$dez = 10;
+$vinte = 20;
+// * / + -
+$resultado = $dez + $vinte;
+echo $resultado;
+
+// operadores de comparação 
+// = 
+// igual == 
+// igual igual ===
+// diferente valor != 
+// diferente valor e tipo !== 
+// maior <
+// menor <
+$valor1 = 5;
+$valor2 = 6;
+$valor3 = 6;
+$teste = $valor1 == $valor2;
+echo $teste;
+$teste1 = $valor2 === $valor3;
+echo $teste1; 
+
+// operadores lógicos
+// &&
+// ||
+// &|
+?>
