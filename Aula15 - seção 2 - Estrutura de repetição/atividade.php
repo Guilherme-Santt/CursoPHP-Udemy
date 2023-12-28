94d8dc1291d111823abf3f0ce7fd43cfbd682633
Guilherme-Santt <guisant@icloud.com>
2023-12-28 09:16:51 -0300

    Atividade Loops

---
+++ Aula15 - seção 2 - Estrutura de repetição/atividade.php
@@ -0,0 +1,12 @@
+<?php 
+$notasalunos = array (4,5,6,7,4,3,6,2,8,8,5,4,3,5,7,8,);
+
+foreach($notasalunos as $notas){
+    if($notas >= 6){
+        echo "<p> Aprovado!</p>";
+    }else{
+        echo "<p>Reprovado</p>"
+    }
+}
+
+?>
